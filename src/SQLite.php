4e3011f7ca89d45113364b6552dbc4a5a44b449b
Antoine Bollinger <antoine.bollinger@gmail.com>
2024-10-15 08:10:17 +0200

    📝 licence

--- src/SQLite.php
+++ src/SQLite.php
@@ -1,4 +1,13 @@
-<?php
+<?php 
+/*
+ * This file is part of the Abollinger\Session package.
+ *
+ * (c) Antoine Bollinger <antoine.bollinger@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Abollinger;
 
 /**
